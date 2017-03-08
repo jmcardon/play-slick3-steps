d055dd66193c32a538a125184f0525694610b024
jmcardon <cardona.josem@gmail.com>
2017-03-08 14:42:38 -0500

    Updated to Slick 3.1 best practices

--- app/models/User.scala
+++ app/models/User.scala
@@ -2,13 +2,9 @@ package models
 
 import javax.inject._
 
-import play.api.Play
-import play.api.Application
 import play.api.data.Form
 import play.api.data.Forms._
-import play.api.db.slick.DatabaseConfigProvider
-
-import slick.backend.DatabaseConfig
+import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
 import slick.driver.JdbcProfile
 import slick.driver.MySQLDriver.api._
 
@@ -44,31 +40,28 @@ class UserTableDef(tag: Tag) extends Table[User](tag, "user") {
 }
 
 @Singleton
-class Users @Inject() (appProvider: Provider[Application]) {
+class Users @Inject() (protected val dbConfigProvider: DatabaseConfigProvider) extends HasDatabaseConfigProvider[JdbcProfile] {
 
-  // This is a hack around legacy code in play-slick.
-  // c.f. https://github.com/playframework/playframework/blob/2.5.4/documentation/manual/releases/release25/migration25/Migration25.md#handling-legacy-components
-  def dbConfig(): DatabaseConfig[JdbcProfile] =
-    DatabaseConfigProvider.get[JdbcProfile](appProvider.get())
+  import driver.api._
 
   val users = TableQuery[UserTableDef]
 
   def add(user: User): Future[String] = {
-    dbConfig().db.run(users += user).map(res => "User successfully added").recover {
+    db.run(users += user).map(res => "User successfully added").recover {
       case ex: Exception => ex.getCause.getMessage
     }
   }
 
   def delete(id: Long): Future[Int] = {
-    dbConfig().db.run(users.filter(_.id === id).delete)
+    db.run(users.filter(_.id === id).delete)
   }
 
   def get(id: Long): Future[Option[User]] = {
-    dbConfig().db.run(users.filter(_.id === id).result.headOption)
+    db.run(users.filter(_.id === id).result.headOption)
   }
 
   def listAll: Future[Seq[User]] = {
-    dbConfig().db.run(users.result)
+    db.run(users.result)
   }
 
 }
